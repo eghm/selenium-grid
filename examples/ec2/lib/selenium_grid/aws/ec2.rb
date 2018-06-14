86bb4c6c616415aca20581f1cc9afc12e247d252
Erik G. H. Meade <eghm@m5>
2018-06-13 17:53:14 -1000

    Microsoft?  No thank-you!

--- examples/ec2/lib/selenium_grid/aws/ec2.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require "yaml"
-require File.dirname(__FILE__) + '/cloud'
-require File.dirname(__FILE__) + '/ec2_client'
-require File.dirname(__FILE__) + '/server'
-require File.dirname(__FILE__) + '/hub'
-require File.dirname(__FILE__) + '/remote_command'
